8739637f71b2c4387e0d349f6234b688ef454c0e
prosperSeb <prospersebille@gmail.com>
2020-03-23 15:41:51 +0100

    feat(movies)

--- express/server.js
+++ express/server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const {PORT} = require('./constants');
+const mongodb = require('./app');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -11,8 +15,17 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+
+router.get('/movies/:id', async(request, response) => {
+  const res = request.params.id;
+  const result = await mongodb.getmovie_id(res);
+  response.send(result);
+});
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
